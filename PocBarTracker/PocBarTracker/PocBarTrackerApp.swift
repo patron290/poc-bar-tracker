46a84c029211d9fda0384bf4fc449f15e06eac10
Balazs Balogh <balazs.bb.balogh@gmail.com>
2024-05-04 14:00:48 +0200

    Initial commit for the App POC.

---
+++ PocBarTracker/PocBarTracker/PocBarTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocBarTrackerApp.swift
+//  PocBarTracker
+//
+//  Created by Balazs on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PocBarTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
