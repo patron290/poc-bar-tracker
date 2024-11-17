aa0ec36b2d87ee3d1c25d7afb4678bfa15e8cc70
BBalazs90 <balazs.bb.balogh@gmail.com>
2024-11-17 09:27:34 +0100

    Rectangle drawing for portrait video works. Still fragile.

--- TestPrediction/Services/OverlayDetection.swift
+++ TestPrediction/Services/OverlayDetection.swift
@@ -6,19 +6,19 @@
 //
 
 import AVFoundation
+import Vision
 import UIKit
 
 func overlayDetectionsOnVideo(
     inputURL: URL, detectionFrames: [DetectionFrame], outputURL: URL
 ) async throws -> URL {
-    // Step 1: Create an AVAsset from the input URL
+
     let originalVideo = AVURLAsset(url: inputURL)
 
-    // Step 2: Create an AVMutableComposition
     let composition = AVMutableComposition()
 
     guard
-        let compositionVideoTrack = composition.addMutableTrack(
+        let originalVideoTrack = composition.addMutableTrack(
             withMediaType: .video,
             preferredTrackID: kCMPersistentTrackID_Invalid)
     else {
@@ -31,7 +31,7 @@ func overlayDetectionsOnVideo(
         let timeRange = CMTimeRange(
             start: .zero, duration: try await originalVideo.load(.duration))
 
-        try compositionVideoTrack.insertTimeRange(
+        try originalVideoTrack.insertTimeRange(
             timeRange,
             of: try await originalVideo.loadTracks(withMediaType: .video)[0],
             at: .zero)
@@ -41,13 +41,19 @@ func overlayDetectionsOnVideo(
 
     let originalSize = try await originalVideo.loadTracks(withMediaType: .video)
         .first!.load(.naturalSize)
-    let duration = try await originalVideo.load(.duration)
+    let transform = try await originalVideo.loadTracks(withMediaType: .video).first!.load(.preferredTransform)
 
+    let width: Int = Int(originalSize.width)
+    let height: Int = Int(originalSize.height)
+    
+    let orientedWidth = transform.a == 0 || transform.d == 0 ? height : width
+    let orientedHeight = transform.a == 0 || transform.d == 0 ? width : height
+    
     let assetWriterSettings =
         [
             AVVideoCodecKey: AVVideoCodecType.h264,
-            AVVideoWidthKey: originalSize.height,
-            AVVideoHeightKey: originalSize.width,
+            AVVideoWidthKey: orientedWidth,
+            AVVideoHeightKey: orientedHeight,
         ] as [String: Any]
 
     do {
@@ -56,13 +62,11 @@ func overlayDetectionsOnVideo(
         print("Could not remove file \(error.localizedDescription)")
     }
 
-    guard let assetwriter = try? AVAssetWriter(outputURL: outputURL, fileType: .mov) else {
+    guard let assetwriter = try? AVAssetWriter(outputURL: outputURL, fileType: .mp4) else {
       abort()
     }
     
-    // ORIENTATION!!!
-    let width: Int = Int(originalSize.height)
-    let height: Int = Int(originalSize.width)
+    
     
     let assetWriterInput = AVAssetWriterInput(mediaType: .video, outputSettings: assetWriterSettings)
     assetWriterInput.expectsMediaDataInRealTime = false
@@ -70,8 +74,8 @@ func overlayDetectionsOnVideo(
     
     let assetWriterAdaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterInput, sourcePixelBufferAttributes: [
         kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32ARGB),
-        kCVPixelBufferWidthKey as String: width,
-        kCVPixelBufferHeightKey as String: height
+        kCVPixelBufferWidthKey as String: orientedWidth,
+        kCVPixelBufferHeightKey as String: orientedHeight
     ])
     
     
@@ -87,8 +91,8 @@ func overlayDetectionsOnVideo(
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         guard let context = CGContext(
             data: nil,
-            width: width,
-            height: height,
+            width: orientedWidth,
+            height: orientedHeight,
             bitsPerComponent: 8,
             bytesPerRow: 0,
             space: colorSpace,
@@ -99,26 +103,26 @@ func overlayDetectionsOnVideo(
         }
         
         context.setFillColor(CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0))
-        context.fill(CGRect(x: 0, y: 0, width: width, height: height))
+        context.fill(CGRect(x: 0, y: 0, width: orientedWidth, height: orientedHeight))
         
         
-        let rect = CGRect(
-                        x: detectionFrame.detection!.minX * originalSize.width,
-                        y: detectionFrame.detection!.minY * originalSize.height,
-                        width: detectionFrame.detection!.width * originalSize.width,
-                        height: detectionFrame.detection!.height * originalSize.height)
+        let newBoundingBox = CGRect(x: detectionFrame.detection?.minX ?? 0, y: 1.0 - (detectionFrame.detection?.maxY ?? 0), width: detectionFrame.detection?.width ?? 0, height: detectionFrame.detection?.height ?? 0)
+        
+        let rect = VNImageRectForNormalizedRect(newBoundingBox,orientedWidth, orientedHeight)
         context.setStrokeColor(UIColor.green.cgColor)
         context.setLineWidth(4)
         context.stroke(rect)
         let cgImage = context.makeImage()!
 
+        
+
         let presentationTime = CMTimeMake(value: Int64(frameIndex), timescale: Int32(framesPerSecond))
         let status = CVPixelBufferPoolCreatePixelBuffer(nil, assetWriterAdaptor.pixelBufferPool!, &pixelBuffer)
         if status == kCVReturnSuccess, let buffer = pixelBuffer {
             CVPixelBufferLockBaseAddress(buffer, .readOnly)
-            let context = CIContext()
+            let cicontext = CIContext()
             let ciImage = CIImage(cgImage: cgImage)
-            context.render(ciImage, to: buffer)
+            cicontext.render(ciImage, to: buffer)
             assetWriterAdaptor.append(buffer, withPresentationTime: presentationTime)
             CVPixelBufferUnlockBaseAddress(buffer, .readOnly)
         } else {
@@ -135,6 +139,34 @@ func overlayDetectionsOnVideo(
       //outputMovieURL now has the video
       print("Finished video location: \(outputURL)")
     }
+    
+    let rectangleVideo = AVURLAsset(url: outputURL)
+    
+    guard
+        let rectangleVideoTrack = composition.addMutableTrack(
+            withMediaType: .video,
+            preferredTrackID: kCMPersistentTrackID_Invalid)
+    else {
+        throw NSError(
+            domain: "Cannot create composition video track", code: 0,
+            userInfo: nil)
+    }
+
+    do {
+        let timeRange = CMTimeRange(
+            start: .zero, duration: try await rectangleVideo.load(.duration))
+
+        try rectangleVideoTrack.insertTimeRange(
+            timeRange,
+            of: try await originalVideo.loadTracks(withMediaType: .video)[0],
+            at: .zero)
+    } catch {
+        throw error
+    }
+    
+    let videoComposition = AVMutableVideoComposition()
+    videoComposition.renderSize = CGSize(width: orientedWidth, height: orientedHeight)
+        videoComposition.frameDuration = CMTimeMake(value: 1, timescale: 30)
 
     return outputURL
 }
